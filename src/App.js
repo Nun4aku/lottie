5e7f387ef70396e0ba35897e9d525804c3512a4a
Nun4aku <gowottmods@gmail.com>
2022-09-12 21:01:38 +0700

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+
+import React, {useState, useRef} from "react";
+import Lottie from "lottie-react";
+import animation from "./animation/INITIALIZE_1_HQ.json";
 import './App.css';
 
 function App() {
+
+  const [go, setGo] = useState(false)
+  const [isLoaded, setIsLoaded] = useState(false)
+  const lottieRef = useRef();
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="parent">
+      <div className="block">
+          {
+            go ? (
+              <Lottie
+                style={{ height: 200 }}
+                lottieRef={lottieRef}
+                animationData = {animation}
+                autoplay = {true}
+                loop= {true}
+              />
+            ) : (
+              <>
+                {
+                  isLoaded ? (
+                    <div>Загрузилось</div>
+                  ) : (
+                    <button
+                      onClick={() => {
+                        setGo(true)
+                        setTimeout(() => {
+                          
+                          setGo(false);
+                          lottieRef.current.pause()
+                          setIsLoaded(true)
+                        }, 3000);
+                      }}
+                  >
+                      ТЫк
+                  </button>
+                  )
+                }
+              </>
+              
+            )
+          }
+      </div>
+    </div>
+
+    
+    
     </div>
   );
 }
 
 export default App;
+
+
